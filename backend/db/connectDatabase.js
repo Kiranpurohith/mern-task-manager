9ec0dc5733a101fe100f714d61eb1b4f8b9f5710
Kiran Purohith <kiranpurohith27@gmail.com>
2026-05-09 16:11:42 +0530

    fix mongo uri typo

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URIr);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("connected to database");
   } catch (error) {
-    console.error(`"Error ${error.message}`);
+    console.error(`Error ${error.message}`);
     process.exit(1);
   }
 };
