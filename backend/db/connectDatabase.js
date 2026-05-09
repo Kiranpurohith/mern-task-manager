a930bed3a3cacd93142fc0d5b813ecc2ec9ae32b
Kiran Purohith <kiranpurohith27@gmail.com>
2026-05-09 16:07:29 +0530

    fix railway production start

--- backend/db/connectDatabase.js
+++ backend/db/connectDatabase.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URIr);
     console.log("connected to database");
   } catch (error) {
     console.error(`"Error ${error.message}`);
