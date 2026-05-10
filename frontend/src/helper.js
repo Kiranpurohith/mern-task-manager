83ec53ef8dae22de563b74480d23f1d4ef66b97e
Kiran Purohith <kiranpurohith27@gmail.com>
2026-05-10 18:01:24 +0530

    fixed backend url

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -4,8 +4,7 @@ import axios from "axios";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 const baseURL =
-  import.meta.env.VITE_BACKEND_BASE_URL || "http://localhost:3000";
-
+  import.meta.env.VITE_BACKEND_BASE_URL || "https://mern-task-manager-z8ra.onrender.com";
 // Axios instance
 const api = axios.create({
   baseURL: `${baseURL}/api/v1`,
